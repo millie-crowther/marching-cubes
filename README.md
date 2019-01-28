32b49714699afeda382edacba2fcf755d6fb27c3
millie <amelia.g.crowther@gmail.com>
2019-01-28 22:15:36 +0000

    removed OOP and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # marching-cubes
+
 just keeping this here in case i need it in the future
+
+no guarantees it will work or even compile (it wont)
--- marching_cubes.h
+++ marching_cubes.h
@@ -1,31 +1,28 @@
 #ifndef MARCHING_CUBES_H
 #define MARCHING_CUBES_H
 
-#include "mesh.h"
-#include "sdf.h"
+namespace marching_cubes {
+    namespace {
+        struct cell_t {
+            float vs[8];
+            vec3_t ps[8];
+        };
 
-class marching_cubes_t {
-private:
-    struct cell_t {
-        float vs[8];
-        vec3_t ps[8];
-    };
+        const std::array<int, 256> edge_table;
+        const int tri_table[256][16];
+        
+        vec3_t interpolate(
+            float isolevel, const vec3_t& a, const vec3_t& b, 
+            float alpha, float beta
+        );
 
-    static const std::array<int, 256> edge_table;
-    static const int tri_table[256][16];
+        void create_mesh_for_cell(
+            float isolevel, const cell_t& cell,
+            std::vector<vertex_t>& vs, std::vector<unsigned int>& is
+        );
+    }
     
-    static vec3_t interpolate(
-        float isolevel, const vec3_t& a, const vec3_t& b, 
-        float alpha, float beta
-    );
-
-    static void create_mesh_for_cell(
-        float isolevel, const cell_t& cell,
-        std::vector<vertex_t>& vs, std::vector<unsigned int>& is
-    );
-
-public:
-    static mesh_t polygonise(
+    mesh_t polygonise(
         float resolution, float isolevel, VkCommandPool pool, 
         VkQueue queue, const sdf_t& sdf
     );
